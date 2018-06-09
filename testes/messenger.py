815f5452398c6fbd62bd1a260e219cd9f574d649
Francis Taylor <francistrapp2000@gmail.com>
2018-06-09 15:41:38 -0300

    Update messenger.py

--- testes/messenger.py
+++ testes/messenger.py
@@ -5,9 +5,8 @@ from cybion import messenger
 
 app = Flask(__name__)
 
-ACCESS_TOKEN = "EAAOO0CLmWNkBAN26ldRcS80zWQZBXv2Xl53lgTew2ZAVYyevd8iSp5C25HRa5e2lueZCBAZAWX8wZBb28qdTRfnQtwO6bZCqg8lI6Yw1HPZBDV9JZAFZC40jg8TqkpaY1fBgIRfZBp7denZAMpG6HpsQgCLV0plw6DcGaJv4AbDbz6YhOMPrhS3qzMa"
+ACCESS_TOKEN = "token_acess"
 VERIFY_TOKEN = "teste"
-
 bot = messenger.Bot(ACCESS_TOKEN)
 
 def test_message(recipient_id):
@@ -25,7 +24,6 @@ def test_gif(recipient_id):
     bot.sendImage(recipient_id, 'https://media.giphy.com/media/rl0FOxdz7CcxO/giphy.gif')
 
 ##### verification 
-
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     if request.method == 'GET':
@@ -33,30 +31,17 @@ def hello():
             return request.args.get("hub.challenge"), 200
         else:
             return 'Invalid verification token', 400
-
-
     if request.method == 'POST':
-        
         data  = request.get_json()['entry'][0]['messaging'][0]
         pprint(data)
-
         if data.get('message'):
             recipient_id = data['sender']['id']
             test_message(recipient_id)
             test_keyborad(recipient_id)
             test_image(recipient_id)
             test_gif(recipient_id)
-            markup= [
-                    dict(title="My Account",type="nested",call_to_actions=[
-                    dict(title="Pay Bill",type="postback",payload="PAYBILL_PAYLOAD"),
-                    dict(type="web_url",title="Latest News",url="https://www.messenger.com/",webview_height_ratio="full")
-                ])]
-            
-            
-             
                   
         return "Success", 200
-
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
