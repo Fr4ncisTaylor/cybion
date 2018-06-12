ab16aa857e7089834e23af5e877ce94fe90cea26
Francis Taylor <francistrapp2000@gmail.com>
2018-06-11 22:51:37 -0300

    Update skeleto.py

--- testes/cybot/skeleto.py
+++ testes/cybot/skeleto.py
@@ -7,7 +7,7 @@ bot = cybot.instance({'token': 'telegram_api_token'})
 
 def handler(api):
   if api.get('text'):
-    bot.sendMessage(99999999, 'Hello World!')
+    bot.sendMessage(api['chat']['id'], 'Hello World!')
 
 bot.start(handler)
 while True:
